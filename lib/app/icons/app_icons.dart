bad811162abd716190241a574c8ed177b61bb371
Lopution <70460628+Lopution@users.noreply.github.com>
2026-08-26 17:10:44 +0800

    feat: add original Pixiv Func icon mappings

---
+++ lib/app/icons/app_icons.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/widgets.dart';
+
+abstract final class AppIcons {
+  static const String _fontFamily = 'iconFont';
+
+  static const addFollow = IconData(0xe900, fontFamily: _fontFamily, matchTextDirection: true);
+  static const filter = IconData(0xe901, fontFamily: _fontFamily, matchTextDirection: true);
+  static const follow = IconData(0xe902, fontFamily: _fontFamily, matchTextDirection: true);
+  static const home = IconData(0xe903, fontFamily: _fontFamily, matchTextDirection: true);
+  static const me = IconData(0xe904, fontFamily: _fontFamily, matchTextDirection: true);
+  static const n = IconData(0xe905, fontFamily: _fontFamily, matchTextDirection: true);
+  static const ranking = IconData(0xe906, fontFamily: _fontFamily, matchTextDirection: true);
+  static const search = IconData(0xe907, fontFamily: _fontFamily, matchTextDirection: true);
+  static const toggle = IconData(0xe908, fontFamily: _fontFamily, matchTextDirection: true);
+  static const pawoo = IconData(0xe909, fontFamily: _fontFamily, matchTextDirection: true);
+  static const twitter = IconData(0xe90a, fontFamily: _fontFamily, matchTextDirection: true);
+  static const web = IconData(0xe90b, fontFamily: _fontFamily, matchTextDirection: true);
+  static const friend = IconData(0xe90c, fontFamily: _fontFamily, matchTextDirection: true);
+  static const blocked = IconData(0xe90d, fontFamily: _fontFamily, matchTextDirection: true);
+}
